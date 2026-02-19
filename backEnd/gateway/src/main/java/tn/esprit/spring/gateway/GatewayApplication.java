c4f14b1272d8c0e1a9099167364b0af5a03779e6
yssfmlha <123382121+yssfmlha@users.noreply.github.com>
2026-02-19 11:11:40 +0100

    added dynamic gateway

--- backEnd/gateway/src/main/java/tn/esprit/spring/gateway/GatewayApplication.java
+++ backEnd/gateway/src/main/java/tn/esprit/spring/gateway/GatewayApplication.java
@@ -3,13 +3,31 @@ package tn.esprit.spring.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+
+		return builder.routes()
+				.route("candidat", r->r.path("/candidats/**")
+						.uri("lb://Candidat"))
+
+				.route("job", r->r.path("/jobs/**")
+						.uri("lb://job"))
+				.route("candidature", r->r.path("/candidatures/**")
+						.uri("lb://candidature"))
+				.route("journal", r->r.path("/journals/**")
+						.uri("lb://journal"))
+				.build();
+	}
 }
